76db889efd4dab31d724fa33b34de54dabb68517
AscahKepha <kephaascah00@gmail.com>
2025-09-15 23:34:54 +0300

    schema file

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,1 +1,10 @@
-import 
+import {relations, sql} from "drizzle-orm"
+import {
+    boolean, integer,
+    pgEnum
+} from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("userType", ["admin", "member", "Clergyman/Clergywoman"])
+
+
+// members table
