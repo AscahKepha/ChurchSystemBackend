cfe60a29e7ad4f5b66c3d827f910c3834aac3633
AscahKepha <kephaascah00@gmail.com>
2025-09-17 16:32:17 +0300

    completing relations

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,6 +1,6 @@
 import {relations, sql} from "drizzle-orm"
 import {
-    boolean, integer,
+    boolean, date, integer,
     pgEnum,
     pgTable,
     serial,
@@ -10,7 +10,10 @@ import {
 } from "drizzle-orm/pg-core";
 
 //Enums
-export const roleEnum = pgEnum("userType", ["admin", "member", "Clergyman/Clergywoman"])
+export const roleEnum = pgEnum("userType", ["admin", "member", "Clergyman/Clergywoman"]);
+export const donationsStatus = pgEnum("donationsStatus", ["pending", "completed", "failed"]);
+export const announcementsStatus = pgEnum("announcementsEnum", ["pending", "active", "inactive" ]);
+
 
 
 //users table
@@ -19,7 +22,7 @@ export const userTable = pgTable("userTable", {
     firstName: varchar("firstName"),
     lastName: varchar("lastName"),
     email: varchar("email"), 
-    psasword: varchar("password").notNull(),
+    pasasword: varchar("password").notNull(),
     role: roleEnum("userType").default("member"),
     address: text("address"),
     contactPhone: varchar("contactPhone", {length:255}),
@@ -31,23 +34,56 @@ export const userTable = pgTable("userTable", {
 //Sermons table
 export const sermonTable = pgTable("sermonTable", {
     sermonId: serial("sermonId").primaryKey(),
+    preacherId: integer("userId").references(()=> userTable.userId, { onDelete: "set null"}),
     title: varchar("title", {length: 255}).notNull(),
-    sermonDate
+    keyVerse: varchar("keyVerse", {length: 255}),
+    sermonDate: date("sermonDate"),
+    speaker: text("speaker"),
     description: text("desription"),
-    audioUrl
-    videoUrl
+    audioUrl: text("audioUrl"),
+    videoUrl: text("videoUrl"),
+    createdAt: timestamp("createdAt").defaultNow(),
+    updatedAt: timestamp("updatedAt").defaultNow(),
 
 })
 //Events table
 export const eventsTable = pgTable("eventsTable", {
-    eventsId:
-    title:
+    eventsId: serial("eventsId").primaryKey(),
+    organizerId: integer("userId").references(()=> userTable.userId, { onDelete: "set null"}),
+    eventDate: date("eventDate"),
+    title: text("title"),
+    description: text("description"),
+    startDate: date("startDate"),
+    endDate: timestamp("endDate"),
+    location: text("location"),
+    imageUrl: text("imageUrl")
     
 
 })
 //Announcements table
+export const aannouncementsTable = pgTable("announcementsTable", {
+    announcementsId: serial("announcementsId").primaryKey(),
+    title: text("title"),
+    content: text("content"),
+    announcementstatus: announcementsStatus("announcementstatus").default("pending"),
+    createdAt: timestamp("createdAt").defaultNow(),
+    updatedAt: timestamp("updatedAt").defaultNow(), 
 
+})
 //Donations/offerings table
-
+export const donationsTable = pgTable("donationsTable", {
+    donationsId: serial("donationsId").primaryKey(),
+    donorId: integer("userId").references(()=> userTable.userId, { onDelete: "set null"}),
+    amount: integer("amount"),
+    donationDate: date("donationDate"),
+    donationstatus: donationsStatus("donationstatus").default("pending"),
+    transactionsId: varchar("transactionsId", {length:255}),
+})
 //Ministries table
-
+export const ministriesTable = pgTable("ministriesTable", {
+    ministryId: serial("ministryId").primaryKey(),
+    name: varchar("name", {length: 255}).notNull(),
+    description: text("description"),
+    leaderId: integer("leaderId").references(()=> userTable.userId, { onDelete: "set null"}),
+    contactInfo: text("contactInfo")
+})
